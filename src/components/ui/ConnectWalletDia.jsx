09d7ca3a700eab5bbe652ef22d00e2600992d59b
chenqiaoda <komorebi_cqd@163.com>
2023-12-24 02:05:12 +0800

    fix: 打包问题解决不了

--- src/components/ui/ConnectWalletDia.jsx
+++ src/components/ui/ConnectWalletDia.jsx
@@ -1,11 +1,11 @@
 import Dialog from '@mui/material/Dialog';
 import PropTypes from 'prop-types';
 import avater from '@/assets/avater.png'
-import useWallet from '@/hooks/useWallet';
+// import useWallet from '@/hooks/useWallet';
 
 
 const ConnectWalletDia = (props) => {
-  const { connectToStarknet } = useWallet();
+//   const { connectToStarknet } = useWallet();
 
     const { onClose, open } = props;
 
@@ -20,10 +20,10 @@ const ConnectWalletDia = (props) => {
                     <div className='my-6 text-3xl'>Connect Wallet</div>
                     <div>Choose a wallet you want to connect. Thereare several wallet providers.</div>
                 </div>
-                <div onClick={() => connectToStarknet()} className='flex items-center justify-center gap-x-3 h-20 cursor-pointer my-10 rounded-xl px-3 w-full shadow-[0px_3px_16px_rgba(47,83,109,0.12)]'>
+                {/* <div onClick={() => connectToStarknet()} className='flex items-center justify-center gap-x-3 h-20 cursor-pointer my-10 rounded-xl px-3 w-full shadow-[0px_3px_16px_rgba(47,83,109,0.12)]'>
                     <img className='flex-shrink-0 w-16 h-16 rounded-3xl' src={avater} alt="" />
                     <div className='flex-1'>Argent</div>
-                </div>
+                </div> */}
                 <div className='flex items-center justify-center gap-x-3 h-20 cursor-pointer rounded-xl px-3 w-full shadow-[0px_3px_16px_rgba(47,83,109,0.12)]'>
                     <img className='flex-shrink-0 w-16 h-16 rounded-3xl' src={avater} alt="" />
                     <div className='flex-1'>Argent</div>
